0d413fd9a6002952de16ff1cc20fc7893fca0f8e
李轻舟 <18903968616@163.com>
2022-09-23 15:21:37 +0800

    feat: 支持点击图标复制链接 🔗

--- routes/github.js
+++ routes/github.js
@@ -1,18 +1,48 @@
-var express = require("express");
-var router = express.Router();
-var axios = require("../public/javascripts/request");
+const fs = require("fs");
+const express = require("express");
+const router = express.Router();
+const axios = require("../public/javascripts/request");
 
-/* GET emojis. */
+/* GET emojis page */
 router.get("/emojis", function (req, res, next) {
+  fs.readFile(
+    "./public/files/emojis.json",
+    { encoding: "utf-8" },
+    (err, data) => {
+      if (err) {
+        res.json(err);
+      } else {
+        res.render("github/emojis", {
+          title: "Github Emojis",
+          emojis: JSON.parse(data),
+          apiLink: "https://api.github.com/emojis",
+        });
+      }
+    }
+  );
+});
+
+/* Update emojis */
+router.put("/emojis", function (req, res, next) {
   axios({ method: "get", url: "https://api.github.com/emojis" })
     .then((response) => {
-      res.render("github/emojis", {
-        title: "Github Emojis",
-        emojis: response.data,
-        apiLink: "https://api.github.com/emojis",
+      const data = response.data;
+      const list = Object.keys(data).map((k) => {
+        return { name: k, url: data[k] };
       });
+      fs.writeFile(
+        "./public/files/emojis.json",
+        JSON.stringify(list),
+        (err) => {
+          if (err) res.json({ status: 200, message: "Fail !" });
+          else {
+            res.json({ status: 200, message: "Success !" });
+          }
+        }
+      );
     })
     .catch((error) => {
+      console.log(error);
       res.json(error);
     });
 });
